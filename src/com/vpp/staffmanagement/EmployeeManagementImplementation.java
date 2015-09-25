654895fcd5e1537dd2d49f1ae4ffce977491b9ce
jarich <jackrichter1@gmail.com>
2015-09-25 11:55:18 +0200

    Ch11 EJB Transactions. The Type SUPPORTS was demonstrated in the dummyInMemoryMethod.

--- src/com/vpp/staffmanagement/EmployeeManagementImplementation.java
+++ src/com/vpp/staffmanagement/EmployeeManagementImplementation.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 
 import com.vpp.dao.EmployeeDaoImpl;
 import com.vpp.staffmanagement.domain.Employee;
@@ -40,5 +42,19 @@ public class EmployeeManagementImplementation implements EmployeeManagementServi
 	public List<Employee> searchBySurname(String surname) {
 		
 		return dao.findBySurname(surname);
+	}
+	
+	/**
+	 * Demonstrates a method that need not be part of the default EJB transactional
+	 * behavior (Propagation Rule) if called from outside directly by a client. Calling it from within
+	 * the Service, keeps the transactional behavior as long as it's code do not 
+	 * communicate with the database (= uses JPA).
+	 * 
+	 * Use this for "In Memory" operations!
+	 */
+	@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+	public double dummyInMemoryMethod()
+	{
+		return 109.3838;
 	}
 }
