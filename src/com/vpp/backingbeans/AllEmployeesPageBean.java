47f7527b23c03675113caaf8084120496c290cc7
jarich <jackrichter1@gmail.com>
2015-10-02 00:51:54 +0200

    Ch15 JSF Components finished. PrimeFaces DataTable component.

--- src/com/vpp/backingbeans/AllEmployeesPageBean.java
+++ src/com/vpp/backingbeans/AllEmployeesPageBean.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIData;
 
 import com.vpp.staffmanagement.EmployeeManagementServiceLocal;
 import com.vpp.staffmanagement.domain.Employee;
@@ -14,6 +15,9 @@ public class AllEmployeesPageBean
 	@EJB
 	private EmployeeManagementServiceLocal employeeService;
 	
+	private UIData dataTable;
+	private Employee selectedEmployee;
+	
 	public List<Employee> getAllEmployees()
 	{
 		return employeeService.getAllEmployees();
@@ -21,6 +25,26 @@ public class AllEmployeesPageBean
 	
 	public String showEmployee()
 	{
+		// Get hold of a reference to the sought employee object via the dataTable object stored in the server
+		this.selectedEmployee = (Employee) this.dataTable.getRowData();
+		
+		// Redirect to the rendering page
 		return "employeeDetails";
+	}
+
+	public UIData getDataTable() {
+		return dataTable;
+	}
+
+	public void setDataTable(UIData dataTable) {
+		this.dataTable = dataTable;
+	}
+
+	public Employee getSelectedEmployee() {
+		return selectedEmployee;
+	}
+
+	public void setSelectedEmployee(Employee selectedEmployee) {
+		this.selectedEmployee = selectedEmployee;
 	}
 }
